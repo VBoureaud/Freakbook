cb2f57f303bdf007e7ca017880d3da21e03a9dcd
VBoureaud <valentin.boureaud@epitech.eu>
2022-06-12 10:21:11 +0200

    Update front - datepicker

--- front/src/scenes/Home.jsx
+++ front/src/scenes/Home.jsx
@@ -1,19 +1,30 @@
-import { Button, message } from "antd";
+import { Button, message, DatePicker, Typography } from "antd";
 import React, { useContext, useEffect, useState } from "react";
 import Web3Context from "../store/web3Context";
 import Web2Context from "../store/web2Context";
 import config from "../config.js";
 
+import { dateToSeconds } from "../utils";
 import { calculateProof } from "../utils/snark";
 
+const { Title } = Typography;
+
 export default function Home(props) {
+  const [date, setDate] = useState(null);
+
   const {
       loading,
   } = useContext(Web3Context);
 
   const handleClick = async () => {
-    const res = await calculateProof();
-    console.log({ res });
+    console.log({ date });
+    if (!date) message.error('Date is wrong or empty.');
+    else {
+      const dateSec = dateToSeconds(date);
+      console.log({ dateSec });
+      const res = await calculateProof(dateSec);
+      console.log({ res });
+    }
   }
 
   return (
@@ -25,8 +36,24 @@ export default function Home(props) {
         minHeight: '70vh',
       }}
     >
-      <h2>Hello Home</h2>
-      <Button onClick={handleClick}>Click Me</Button>      
+      
+      <div style={{
+        maxWidth: '450px',
+        margin: 'auto',
+      }}>
+        <Title>What's your birthdate ?</Title>
+        <div style={{
+          display: 'flex'
+        }}>
+          <DatePicker
+            /*disabledDate={(current) => current && current < moment.today()}*/
+            onChange={(date, dateString) => setDate(dateString)}
+            style={{ flex: 3 }}
+          />
+          <Button type='primary' style={{ flex: 1 }} onClick={handleClick}>Confirm</Button>   
+        </div>
+      </div>
+      
       {loading && (
         <Button type="primary" style={{ margin: 'auto' }} loading>
           Loading
--- front/src/utils/snark.js
+++ front/src/utils/snark.js
@@ -2,32 +2,47 @@
 //import wasm_hasher from "./zkeys/Hasher_2-1.test.wasm";
 //import birthdate from "./zkeys/birthdate.wasm";
 //import key_hasher from "./zkeys/Hasher_2-1.test.zkey";
-import verification_key from "./zkeys/verification_key.json";
+//import verification_key from "./zkeys/verification_key.json";
+import { BigNumber, utils } from "ethers";
 
 if (typeof module !== "undefined") var snarkjs = require('snarkjs')
 
-export const calculateProof = async () => {
+export const calculateProof = async (dateSeconds) => {
     const start = Date.now();
     console.log('start calculateProof');
+    const time = new Date().getTime() / 1000;
+    const nowSeconds = parseInt(time);
+    console.log({ nowSeconds });
+    console.log({ dateSeconds });
+    const bigIntNow = BigNumber.from(nowSeconds);
+    const bigIntBirthDate = BigNumber.from(dateSeconds);
+    console.log(utils.hexlify(utils.arrayify(bigIntBirthDate)));
+    console.log(utils.hexlify(utils.arrayify(bigIntNow)));
+    // timestamp date
+    // timestamp current
     const { proof, publicSignals } =
-
-      await snarkjs.plonk.fullProve( {in: [0, 0]}, "http://0.0.0.0:4765/public/birthdate.wasm", "http://0.0.0.0:4765/public/Hasher_2-1.test.zkey");
+      await snarkjs.groth16.fullProve({
+        'birthdate': utils.hexlify(utils.arrayify(bigIntBirthDate)),
+        'referenceTime': utils.hexlify(utils.arrayify(bigIntNow)),
+      },
+      "http://0.0.0.0:4765/public/birthdate.wasm",
+      "http://0.0.0.0:4765/public/birthdate_0001.zkey");
 
     const end = Date.now();
     const timeTaken = ((end - start) / 1000).toString() + ' seconds';
-    console.log('step 1/3');
+    console.log({proof});
+    console.log({publicSignals});
 
     //timeComponent.innerHTML = timeTaken;
 
     //proofCompnent.innerHTML = JSON.stringify(proof, null, 1);
 
-    console.log('fetch verification_key');
+    /*console.log('fetch verification_key');
     const vkey = await fetch(verification_key).then( function(res) {
         return res.json();
     });
     console.log('fetched, go plonk');
 
-    const res = await snarkjs.plonk.verify(vkey, publicSignals, proof);
-    console.log({ res });
-    return res;
+    const res = await snarkjs.plonk.verify(vkey, publicSignals, proof);*/
+    return true;
 }
